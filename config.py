0c26bf7f82622ced37e24cb6748727d2165f8c2d
notxkrishnaa <krishnaonly999@gmail.com>
2025-10-30 17:10:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MONGO_URI: str = os.getenv("MONGO_URI", "mongodb+srv://rj5706603:O95nvJYxapyDHfkw@cluster0.fzmckei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
     # Pyrogram session name
-    SESSION_NAME: str = os.getenv("SESSION_NAME", "1BVtsOJ0Bu6yUAF7S2Gg843d8LG_piuRM_VP-zI2Fa6ZBz7ZgRj1hqW1qU0GYYn0r0aA13qvCMS9N7OoiyOQCGy2mdmJh_AWghEQZJabmh-1Kq5p_z2mvTztXIOXBSySjnGTVhc-yQUYO3XIf-ce2__sbH2kqWJVe-8SP5ze3U6MPowZcYXKRaDETfgmjq7qZ5KWtpiY5UMVQ63FwHgXZ4UfGWdig6Ry0IuKiZDul6HWu0xDoHctRsndY29SlXm-Cez391wcIeh4aQp6Le6ii4HMwpq5L8_gVVyAqkwaN-uYohCb-dkzk104yzORpESa_ho5CSZSQoRTAZPgII_f9X-Crac9np-k=")
+    SESSION_NAME: str = os.getenv("SESSION_NAME", "")
     
     # Other settings
     MAX_DURATION: int = 3600  # Max song duration in seconds (1 hour)
