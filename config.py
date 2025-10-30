89da8ec965b7af2a86f2c4bf9bc3414de4c225db
notxkrishnaa <krishnaonly999@gmail.com>
2025-10-30 17:07:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from typing import Optional
 
 class Config:
     # Telegram API credentials - Yahan apne actual values fill karein
-    API_ID: int = int(os.getenv("API_ID", "1234567"))  # Apna API ID yahan daalein
-    API_HASH: str = os.getenv("API_HASH", "your_api_hash_here")  # Apna API Hash yahan daalein
-    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11")  # Apna Bot Token yahan daalein
+    API_ID: int = int(os.getenv("API_ID", "23640310"))  # Apna API ID yahan daalein
+    API_HASH: str = os.getenv("API_HASH", "079f8339732e35e032a64ee020e0b90b")  # Apna API Hash yahan daalein
+    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "8458860951:AAGnhMc3ATeiHRo43Q4ZDpz5tjPV1rygnsM")  # Apna Bot Token yahan daalein
     
     # MongoDB - Apna MongoDB connection string yahan daalein
     MONGO_URI: str = os.getenv("MONGO_URI", "mongodb+srv://username:password@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority")
